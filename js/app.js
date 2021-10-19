b557f5b3025b6011d7b5b710b39c37f16d411caa
mostafa mansour <mostafayasser6102001@gmail.com>
2021-10-19 18:30:54 +0200

    second commit

--- js/app.js
+++ js/app.js
@@ -13,6 +13,43 @@
  * 
 */
 
+
+
+const navBarList = document.getElementById("navbar__list");
+
+const dataNav = document.querySelectorAll("[data-nav]");
+
+
+for (let i = 0; i < dataNav.length; i++) {
+  const navDataName = dataNav[i].getAttribute("data-nav");
+  
+  const navId = dataNav[i].getAttribute("id");
+  
+  const newList = document.createElement("li");
+  newList.classList.add(`menu__link`);
+  
+  const newLink = document.createElement("a");
+  newLink.setAttribute("href", `#${navId}`)
+  newLink.textContent = navDataName;
+  
+  newList.appendChild(newLink);
+  navBarList.appendChild(newList);
+  
+  console.log(navDataName);
+  console.log(navId);
+  console.log(newList);
+}
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
