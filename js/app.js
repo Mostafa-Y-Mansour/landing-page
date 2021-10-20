35cf02ab5c87a771bd2e2a9825a9e19fab0fdcc8
mostafa mansour <mostafayasser6102001@gmail.com>
2021-10-20 23:52:54 +0200

    third commit

--- js/app.js
+++ js/app.js
@@ -1,99 +1,69 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
-
-
+// select The ul Element with id of "#navbar__list" //
 const navBarList = document.getElementById("navbar__list");
 
+// select All Section With An Attribute "[data-nav]" //
 const dataNav = document.querySelectorAll("[data-nav]");
 
+// Creat a Fragment //
+const fragment = document.createDocumentFragment();
 
+// A for loop to loop over every section //
 for (let i = 0; i < dataNav.length; i++) {
   const navDataName = dataNav[i].getAttribute("data-nav");
   
-  const navId = dataNav[i].getAttribute("id");
-  
   const newList = document.createElement("li");
   newList.classList.add(`menu__link`);
-  
-  const newLink = document.createElement("a");
-  newLink.setAttribute("href", `#${navId}`)
-  newLink.textContent = navDataName;
-  
-  newList.appendChild(newLink);
-  navBarList.appendChild(newList);
-  
-  console.log(navDataName);
-  console.log(navId);
-  console.log(newList);
+  newList.textContent = navDataName;
+
+  fragment.appendChild(newList);
 }
 
+// Add the created "fragment" to the "ul" Element //
+navBarList.appendChild(fragment);
 
+// Add Scroll Animation //
+for (let i = 0; i < dataNav.length; i++) {
+  const list = document.querySelectorAll("li");
+  list[i].addEventListener("click", function () {
+    dataNav[i].scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline: "nearest",
+    });
+  });
+}
 
-
-
-
-
-
-
-
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
+window.addEventListener("scroll", () => {
+  for (let i = 0; i < dataNav.length; i++) {
+    const rect = dataNav[i].getBoundingClientRect();
+    const menuLink = document.getElementsByClassName("menu__link");
+    if (
+      rect.top < window.innerHeight - 150 &&
+      rect.top >=  - 50
+    ) {
+      dataNav[i].classList.add("your-active-class");
+      menuLink[i].classList.add("active");
+      
+    } else {
+      dataNav[i].classList.remove("your-active-class");
+      menuLink[i].classList.remove("active");
+    }
+  }
+});
 
 
